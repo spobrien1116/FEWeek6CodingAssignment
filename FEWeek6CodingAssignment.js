610f62d50fc0f3c9422deeba53b305a2fd38c519
spobrien1116 <87784429+spobrien1116@users.noreply.github.com>
2022-07-06 18:54:12 -0400

    Changes before office hours
    Properly separated out card and deck creation: working fine. Player creation has also mostly been completed. Tried adding randomizer: currently commented out, not working as intended. Need to fix that issue then move on to scoring.

--- FEWeek6CodingAssignment.js
+++ FEWeek6CodingAssignment.js
@@ -35,6 +35,34 @@ class Card {
 
 // Class designed to create a deck of all 52 cards.
 class Deck {
+    
+    constructor() {
+        this.deck = [];
+    }
+
+    createDeck() {
+        for (let i = 2; i < 15; i++) {
+            let cardSpade = new Card(i, 'Spades');
+            this.deck.push(cardSpade.cardCreator());
+        }
+
+        for (let i = 2; i < 15; i++) {
+            let cardHeart = new Card(i, 'Hearts');
+            this.deck.push(cardHeart.cardCreator());
+        }
+
+        for (let i = 2; i < 15; i++) {
+            let cardClub = new Card(i, 'Clubs');
+            this.deck.push(cardClub.cardCreator());
+        }
+
+        for (let i = 2; i < 15; i++) {
+            let cardDiamond = new Card(i, 'Diamonds');
+            this.deck.push(cardDiamond.cardCreator());
+        }
+
+        return this.deck;
+    }
 
 }
 
@@ -47,8 +75,24 @@ class Player {
         this.playerScore = 0;
     }
 
-    createPlayer() {
+    createPlayer(randomDeck) {
+        let firstHalfOfDeck = [];
+        let secondHalfOfDeck = [];
         console.log(`${this.player} has been created. They start off with a score of ${this.playerScore} points.`);
+        if (this.player === "Player 1") {
+            for (let i = 0; i < 26; i++) {
+                firstHalfOfDeck.push(randomDeck[i]);
+            }
+            console.log(this.player + "'s starting deck:");
+            console.log(firstHalfOfDeck);
+        }
+        if (this.player === "Player 2") {
+            for (let i = 26; i < 52; i++) {
+                secondHalfOfDeck.push(randomDeck[i]);
+            }
+            console.log(this.player + "'s starting deck:");
+            console.log(secondHalfOfDeck);
+        }
     }
 
 }
@@ -59,25 +103,34 @@ class Game {
     
     constructor() {
         this.deck = [];
-
     }
-
-    createCards() {
-        for (let i = 2; i < 14; i++) {
-            let cardSpade = new Card(i, 'Spades');
-            console.log(cardSpade.cardCreator());
-        }
+    
+    createCardsAndDeck() {    
+        let createTheCardsAndDeck = new Deck();
+        let newDeck = (createTheCardsAndDeck.createDeck());
+        // This should show the non-shuffled deck.
+        console.log("Here is the created deck, unshuffled.");
+        console.log(newDeck);
+        this.deck = newDeck;
+        // This part is included to randomize the array of cards that were just created.
+        // for (let i = this.deck.length - 1; i > 0; i--) {
+        //     let r = Math.floor(Math.random() * (i + 1));
+        //     let temp = this.deck[i];
+        //     this.deck[i] = this.deck[r];
+        //     this.deck[r] = temp;
+        // }
+        // console.log("Here is the new randomized deck. It consists of cards in this order: " + this.deck);
     }
     
     createPlayers() {
         let player1 = new Player("Player 1");
         let player2 = new Player("Player 2");
-        player1.createPlayer();
-        player2.createPlayer();
+        player1.createPlayer(this.deck);
+        player2.createPlayer(this.deck);
     }
 
 }
 
 let game = new Game();
-game.createCards();
+game.createCardsAndDeck();
 game.createPlayers();
